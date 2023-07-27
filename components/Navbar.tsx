7f75baf276c432b284a8ffb625cc722357ebb4d9
kd5200 <kareemdavis18@yahoo.com>
2023-07-27 17:58:17 -0400

    finishing touches

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,12 +4,15 @@ import { animate, motion } from "framer-motion";
 import { logo } from "@/public/assets";
 import Image from "next/image";
 import { MdOutlineClose } from "react-icons/md";
+import { TbBrandGithub, TbBrandTwitter } from "react-icons/tb";
+import { SlSocialLinkedin, SlSocialInstagram } from "react-icons/sl";
 
 const Navbar = () => {
   const ref = useRef<string | any>("");
   const [showMenu, setShowMenu] = useState(false);
   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
+    setShowMenu(false);
     const href = e.currentTarget.href;
     const targetId = href.replace(/.*\#/, "");
     const elem = document.getElementById(targetId);
@@ -22,6 +25,12 @@ const Navbar = () => {
     });
     e.currentTarget.classList.add("active");
   };
+
+  function handleClick(e: any) {
+    if (e.target.contains(ref.current)) {
+      setShowMenu(false);
+    }
+  }
 
   return (
     <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bodyColor px-4">
@@ -125,8 +134,9 @@ const Navbar = () => {
         {showMenu && (
           <div
             ref={(node) => (ref.current = node)}
-            className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black bg-opacity-50 flex flex-col items-end"
             onClick={handleClick}
+            className="absolute mdl:hidden top-0 right-0 w-full h-screen bg-black bg-opacity-50 flex flex-col items-end"
+            // onClick={handleClick}
           >
             <motion.div
               initial={{ x: 20, opacity: 0 }}
@@ -138,6 +148,116 @@ const Navbar = () => {
                 onClick={() => setShowMenu(false)}
                 className="text-3xl text-textGreen cursor-pointer hover:text-red-500 absolute top-4 right-4"
               />
+              <div className="flex flex-col items-center gap-7">
+                <ul className="flex flex-col text-base gap-7">
+                  <Link
+                    href="#home"
+                    onClick={handleScroll}
+                    className="flex items-center  font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{ duration: 0.2, delay: 0.1, ease: "easeIn" }}
+                    >
+                      Home
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#about"
+                    onClick={handleScroll}
+                    className="flex items-center  font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{ duration: 0.2, delay: 0.2, ease: "easeIn" }}
+                    >
+                      <span className="text-textGreen">01.</span>About
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#experience"
+                    onClick={handleScroll}
+                    className="flex items-center font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{ duration: 0.2, delay: 0.3, ease: "easeIn" }}
+                    >
+                      <span className="text-textGreen">02.</span>Experience
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#projects"
+                    onClick={handleScroll}
+                    className="flex items-center  font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ x: 20, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      transition={{ duration: 0.2, delay: 0.4, ease: "easeIn" }}
+                    >
+                      <span className="text-textGreen">03.</span>Projects
+                    </motion.li>
+                  </Link>
+                  <Link
+                    href="#contact"
+                    onClick={handleScroll}
+                    className="flex items-center  font-medium text-textDark hover:text-textGreen cursor-pointer duration-300 nav-link"
+                  >
+                    <motion.li
+                      initial={{ y: -10, opacity: 0 }}
+                      animate={{ y: 0, opacity: 1 }}
+                      transition={{ duration: 0.1, delay: 0.1 }}
+                    >
+                      <span className="text-textGreen">04.</span>Contact
+                    </motion.li>
+                  </Link>
+                </ul>
+                <a href="/assets/resume.pdf" target="_blank">
+                  <motion.button
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.5 }}
+                    className=" text-textGreen px-4 py-2 rounded-md text-[13px] border border-textGreen hover:bg-hoverColor duration-300"
+                  >
+                    Resume
+                  </motion.button>
+                </a>
+                <div>
+                  <motion.a href="https://github.com/kd5200" target="_blank">
+                    <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                      <TbBrandGithub />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    href="https://www.linkedin.com/in/kareem-d-b69a8a105/"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                      <SlSocialLinkedin />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    href="https://twitter.com/King_KD_10"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                      <TbBrandTwitter />
+                    </span>
+                  </motion.a>
+                  <motion.a
+                    href="https://www.instagram.com/askari_ll/"
+                    target="_blank"
+                  >
+                    <span className="w-10 h-10 text-xl bg-bodyColor border-[1px] border-zinc-700 hover:border-textGreen text-zinc-200 rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:-translate-y-2 transition-all duration-300">
+                      <SlSocialInstagram />
+                    </span>
+                  </motion.a>
+                </div>
+              </div>
             </motion.div>
           </div>
         )}
