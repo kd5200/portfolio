44b9d6ec4efe135fc22efadf546a8bddf1a45798
kd5200 <kareemdavis18@yahoo.com>
2024-11-12 11:40:50 -0500

    gtm tag implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Initializing GTM */}
+      <GoogleTagManager gtmId="GTM-KMJS572M" />
+
+      {/* Rendering the page component after */}
+      <Component {...pageProps} />
+    </>
+  );
 }
